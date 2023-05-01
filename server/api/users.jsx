573766b491333f62bd39275d518cbea3d784d717
CJ <cliftonjones.87@gmail.com>
2023-04-30 23:52:45 -0400

    feature: defined CRUD routes for users

---
+++ server/api/users.jsx
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    console.log('error retrieving all users');
+    next(error);
+  }
+});
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(+req.params.userId);
+    res.json(user);
+  } catch (error) {
+    console.log('error retrieving single user');
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const createdUser = await User.create({ ...req.body });
+    res.status(201).json(createdUser);
+  } catch (error) {
+    console.log('error creating single user');
+    next(error);
+  }
+});
+
+router.put('/:userId', async (req, res, next) => {
+  try {
+    const userBeingUpdated = await User.findByPk(+req.params.userId);
+    const wasUpdated = await userBeingUpdated.update({ ...req.body });
+    res.json(wasUpdated);
+  } catch (error) {
+    console.log('error updating single user');
+    next(error);
+  }
+});
+
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    const userBeingDeleted = await User.findByPk(+req.params.userId);
+    const wasDeleted = await userBeingDeleted.destroy({ ...req.body });
+    res.json(wasDeleted);
+  } catch (error) {
+    console.log('error deleting single user');
+    next(error);
+  }
+});
+
+module.exports = router;
