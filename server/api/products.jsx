42d5e3955d3d0c44df1c8f7d66ae4b025f998865
CJ <cliftonjones.87@gmail.com>
2023-04-30 23:50:23 -0400

    feature: defined CRUD routes for products

---
+++ server/api/products.jsx
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = Product.findAll();
+    res.json(products);
+  } catch (error) {
+    console.log('error retrieving all products');
+    next(error);
+  }
+});
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = Product.findByPk(+req.params.productId);
+    res.json(product);
+  } catch (error) {
+    console.log('error retrieving single product');
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const createdProject = await Project.create({ ...req.body });
+    res.status(201).json(createdProject);
+  } catch (error) {
+    console.log('error creating single product');
+    next(error);
+  }
+});
+
+router.put('/:productId', async (req, res, next) => {
+  try {
+    const projectBeingUpdated = await Project.findByPk(+req.params.productId);
+    const wasUpdated = await projectBeingUpdated;
+    res.json(wasUpdated);
+  } catch (error) {
+    console.log('error updating single product');
+    next(error);
+  }
+});
+
+router.delete('/:projectId', async (req, res, next) => {
+  try {
+    const projectBeingDeleted = await Project.findByPk(+req.params.projectId);
+    const wasDeleted = await projectBeingDeleted.destroy({ ...req.body });
+    res.json(wasDeleted);
+  } catch (error) {
+    console.log('error deleting single product');
+    next(error);
+  }
+});
+
+module.exports = router;
