eddd824d821a9271478d3e8375937eb3a2145b05
CJ <cliftonjones.87@gmail.com>
2023-05-01 11:47:21 -0400

    chore: created sequelize instance

---
+++ server/db/db.jsx
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    'postgres://localhost:5432/theblueprintboilerplate',
+  {
+    logging: false,
+  }
+);
+
+module.exports = db;
