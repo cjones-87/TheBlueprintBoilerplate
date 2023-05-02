5020cd27164d3bec7a4a395dbce45a6df4394f95
CJ <cliftonjones.87@gmail.com>
2023-05-02 00:11:02 -0400

    chore: sync and initialize database

--- server/index.jsx
+++ server/index.jsx
@@ -1,9 +1,18 @@
+const db = require('./db');
 const app = require('./app');
-
 const port = process.env.port || 3000;
 
-app.listen(port, () => {
-  console.log('Knock, knock');
-  console.log("Who's there?");
-  console.log(`Your server, listening on port ${port}`);
-});
+const init = async () => {
+  try {
+    await db.sync().then(() => {
+      console.log('Knock, knock');
+      console.log("Who's there?");
+      console.log(`Your server, listening on port ${port}`);
+      app.listen(port);
+    });
+  } catch (error) {
+    console.log('There was an error initializing the app');
+  }
+};
+
+init();
