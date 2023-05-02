1d6f7cfc422bc77c92907b84e09d37b1971f3815
CJ <cliftonjones.87@gmail.com>
2023-05-02 02:08:57 -0400

    feature: added client-side entry point

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = document.getElementById('root');
+
+createRoot(root).render(
+  <div>
+    <h1>CJ's Blueprint Boilerplate</h1>
+  </div>
+);
