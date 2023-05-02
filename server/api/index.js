cde1ec271334f3a9a95bcdc73a8c1ab3ee287a9a
CJ <cliftonjones.87@gmail.com>
2023-05-02 13:35:45 -0400

    fix: corrected mount path

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-router.use('users', require('./users'));
-router.use('products', require('./products'));
+router.use('/users', require('./users'));
+router.use('/products', require('./products'));
 
 router.use((req, res, next) => {
   const err = new Error('404 was not found');
