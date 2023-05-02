0f919674013dab89abdf6722467e45c3141c4ecb
CJ <cliftonjones.87@gmail.com>
2023-05-02 00:07:44 -0400

    feature: added additional fields for user model

--- server/db/models/User.jsx
+++ server/db/models/User.jsx
@@ -11,6 +11,58 @@ const User = db.define('user', {
     type: Sequelize.STRING,
     allowNull: false,
   },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true },
+  },
+  username: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  billingAddress: {
+    type: Sequelize.STRING,
+  },
+  shippingAddress: {
+    type: Sequelize.STRING,
+  },
+  billingCity: {
+    type: Sequelize.STRING,
+  },
+  shippingCity: {
+    type: Sequelize.STRING,
+  },
+  billingPostalCode: {
+    type: Sequelize.INTEGER,
+    validate: {
+      is: /^\d{5}(-\d{4})?$/,
+    },
+  },
+  shipppingPostalCode: {
+    type: Sequelize.INTEGER,
+    validate: {
+      is: /^\d{5}(-\d{4})?$/,
+    },
+  },
+  phone: {
+    type: Sequelize.STRING,
+    validate: {
+      is: /^((1-)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4})$/,
+    },
+  },
+  profileAvatar: {
+    type: Sequelize.STRING,
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
 });
 
 module.exports = User;
