b503c5497c70202e2299fbfcffd96fa1a4616b26
CJ <cliftonjones.87@gmail.com>
2023-05-04 01:25:20 -0400

    feature: served up frontend navigation routes to app, served up app to index.jsx entry point

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import App from './app.jsx';
 
 const root = document.getElementById('root');
 
 createRoot(root).render(
   <div>
     <h1>CJ's Blueprint Boilerplate</h1>
+    <App />
   </div>
 );
