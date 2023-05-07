f92891bba8e1239022419d8451f684d3f2f3e180
CJ <cliftonjones.87@gmail.com>
2023-05-06 21:50:01 -0400

    feature: added registration form data

---
+++ src/forms/formData/authentication/RegistrationFormData.js
@@ -0,0 +1,9 @@
+const RegistrationFormData = [
+  { label: 'First Name: ', type: 'text' },
+  { label: 'Last Name: ', type: 'text' },
+  { label: 'Username: ', type: 'text' },
+  { label: 'Password: ', type: 'text' },
+  { label: 'Email: ', type: 'email' },
+];
+
+export default RegistrationFormData;
