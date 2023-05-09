6b52a451c7782474454e7af51e71ed6a78af73b4
CJ <cliftonjones.87@gmail.com>
2023-05-09 00:05:55 -0400

    chore: added static middleware for serving public folder

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '..', '/dist')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
