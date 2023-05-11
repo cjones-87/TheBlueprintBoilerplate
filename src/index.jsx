f1a620c3ecea32a79dac7591de2a47c5a09f1f47
CJ <cliftonjones.87@gmail.com>
2023-05-10 23:44:09 -0400

    chore: wrapped app with provider to provide access to store

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React, { lazy } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store';
 import './index.css';
 import App from './app.jsx';
 
 const root = document.getElementById('root');
 
 createRoot(root).render(
-  <div>
+  <Provider store={store}>
     <App />
-  </div>
+  </Provider>
 );
