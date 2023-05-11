1596af1b2b32f6f11f449000ae544ba188cfb9af
CJ <cliftonjones.87@gmail.com>
2023-05-11 12:20:16 -0400

    chore: added name field for form data

--- src/forms/formData/authentication/RegistrationFormData.js
+++ src/forms/formData/authentication/RegistrationFormData.js
@@ -1,9 +1,29 @@
 const RegistrationFormData = [
-  { autoComplete: 'given-name', label: 'First Name: ', type: 'text' },
-  { autoComplete: 'family-name', label: 'Last Name: ', type: 'text' },
-  { autoComplete: 'username', label: 'Username: ', type: 'text' },
-  { autoComplete: 'new-password', label: 'Password: ', type: 'password' },
-  { autoComplete: 'email', label: 'Email: ', type: 'email' },
+  {
+    autoComplete: 'given-name',
+    label: 'First Name: ',
+    name: 'firstName',
+    type: 'text',
+  },
+  {
+    autoComplete: 'family-name',
+    label: 'Last Name: ',
+    name: 'lastName',
+    type: 'text',
+  },
+  {
+    autoComplete: 'username',
+    label: 'Username: ',
+    name: 'username',
+    type: 'text',
+  },
+  {
+    autoComplete: 'new-password',
+    label: 'Password: ',
+    name: 'password',
+    type: 'password',
+  },
+  { autoComplete: 'email', label: 'Email: ', name: 'email', type: 'email' },
 ];
 
 export default RegistrationFormData;
