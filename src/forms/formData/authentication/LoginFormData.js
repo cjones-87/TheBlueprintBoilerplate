a23f5d2fdb11cbed2a51c01dd2b2965feba9ef09
CJ <cliftonjones.87@gmail.com>
2023-05-11 23:54:17 -0400

    feature: added name attribute for login form processing

--- src/forms/formData/authentication/LoginFormData.js
+++ src/forms/formData/authentication/LoginFormData.js
@@ -1,7 +1,17 @@
 const LoginFormData = [
-  { autoComplete: 'email', label: 'Email: ', type: 'email' },
-  { autoComplete: 'username', label: 'Username: ', type: 'text' },
-  { autoComplete: 'current-password', label: 'Password: ', type: 'password' },
+  { autoComplete: 'email', label: 'Email: ', name: 'email', type: 'email' },
+  {
+    autoComplete: 'username',
+    label: 'Username: ',
+    name: 'username',
+    type: 'text',
+  },
+  {
+    autoComplete: 'current-password',
+    label: 'Password: ',
+    name: 'password',
+    type: 'password',
+  },
 ];
 
 export default LoginFormData;
