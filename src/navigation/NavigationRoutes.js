a8b01a6d022b2c228c806291edacf2638eeab7bf
CJ <cliftonjones.87@gmail.com>
2023-05-12 00:28:46 -0400

    feature: wrapped navbar components around routes

--- src/navigation/NavigationRoutes.js
+++ src/navigation/NavigationRoutes.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FrontendRouteData from './FrontendRouteData';
+
+const Navbar = lazy(() => import('../components/navbar/Navbar'));
 
 const NavigationRoutes = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        {FrontendRouteData.map((route, index) => (
-          <Route element={route.element} key={index} path={route.path} />
-        ))}
-      </Routes>
+      <Navbar>
+        <Routes>
+          {FrontendRouteData.map((route, index) => (
+            <Route element={route.element} key={index} path={route.path} />
+          ))}
+        </Routes>
+      </Navbar>
     </BrowserRouter>
   );
 };
