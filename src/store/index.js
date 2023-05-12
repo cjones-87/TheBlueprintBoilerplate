40e3472a4c9d1d2894639af26344b6b62fbe5e12
CJ <cliftonjones.87@gmail.com>
2023-05-11 22:56:48 -0400

    feature: added auth reducer to redux store

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,11 @@ import { createLogger } from 'redux-logger';
 import { applyMiddleware } from 'redux';
 import productsReducer from './products/products';
 import usersReducer from './users/users';
+import authReducer from './auth/auth';
 import thunkMiddleware from 'redux-thunk';
 
 const store = configureStore(
-  { reducer: { productsReducer, usersReducer } },
+  { reducer: { authReducer, productsReducer, usersReducer } },
   applyMiddleware(thunkMiddleware, createLogger())
 );
 
