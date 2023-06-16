c9e525654a44037dae7a394547e0101ce463f968
CJ <cliftonjones.87@gmail.com>
2023-06-16 13:09:41 -0400

    feature: added custom hook to handle window resizes

---
+++ src/misc/customHooks/useWindowDimensions.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+const useWindowDimensions = () => {
+  const [dimensions, setDimensions] = useState({
+    height: innerHeight,
+    width: innerWidth,
+  });
+
+  useEffect(() => {
+    const handleResize =
+      dimensions.width < 768
+        ? debounce(
+            () => setDimensions({ height: innerHeight, width: innerWidth }),
+            100
+          )
+        : debounce(() =>
+            setDimensions({ height: innerHeight, width: innerWidth })
+          );
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [dimensions.height, dimensions.width]);
+
+  return dimensions;
+};
+export default useWindowDimensions;
