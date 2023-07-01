6c7ee61d1b1cba000549a471370c8c6897034fff
CJ <cliftonjones.87@gmail.com>
2023-06-30 23:36:29 -0400

    feature: added line order model to serve as line items on fulfilled and unfulfilled orders/carts

---
+++ server/db/models/LineItem.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const LineItem = db.define('LineItem', {
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+});
+
+module.exports = LineItem;
