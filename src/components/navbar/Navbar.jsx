3c281a9c8c14692a89ff5c9f9962adeb1ce97ec1
CJ <cliftonjones.87@gmail.com>
2023-07-03 22:58:22 -0400

    refactor: updated navbar component to accept props, refactored ul to map through navlinks instead of hardcoding

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,26 @@
 import React, { useCallback, useState } from 'react';
 import useWindowDimensions from '../../misc/customHooks/useWindowDimensions';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { logout } from '../../store/auth/auth';
-import NavbarLogo from './NavbarLogo';
 
-const Navbar = ({ children }) => {
-  const { height, width } = useWindowDimensions();
+const Navbar = ({ end, navLinks, start }) => {
+  const { width } = useWindowDimensions();
   const [burgerActive, setBurgerActive] = useState(false);
-  const auth = useSelector((state) => state.authReducer);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleBurgerClick = useCallback(() => {
     setBurgerActive((current) => !current);
   }, []);
 
-  const handleNavLinkClick = useCallback(() => {
-    setBurgerActive((current) => !current);
-  }, []);
+  const handleNavLinkClick = useCallback(
+    (href) => {
+      navigate(href);
+      setBurgerActive((current) => !current);
+    },
+    [navigate]
+  );
 
   const handleLogout = () => {
     try {
@@ -29,59 +33,39 @@ const Navbar = ({ children }) => {
   return (
     <div className="navContainer" style={{ width }}>
       <nav className="navbar">
-        <a className="navbarLogo" href="/">
-          <NavbarLogo />
-        </a>
+        <div id="navbarStart">
+          {start}
+          <ul className={`navMenu ${burgerActive ? 'active' : ''}`}>
+            {navLinks.map((data, index) => (
+              <li
+                className="navItem"
+                key={index}
+                onClick={data.content === 'Logout' ? handleLogout : null}
+              >
+                <button
+                  className="navLink button"
+                  onClick={() => handleNavLinkClick(data.href)}
+                >
+                  {data.content}
+                </button>
+              </li>
+            ))}
+          </ul>
 
-        <ul className={`navMenu ${burgerActive ? 'active' : ''}`}>
-          {auth && auth.isAuthenticated ? (
-            <>
-              <li className="navItem">
-                <a className="navLink" href="/" onClick={handleNavLinkClick}>
-                  Home
-                </a>
-              </li>
-              <li className="navItem" onClick={handleLogout}>
-                <a className="navLink" href="/" onClick={handleNavLinkClick}>
-                  Logout
-                </a>
-              </li>
-            </>
-          ) : (
-            <>
-              <li className="navItem">
-                <a
-                  className="navLink"
-                  href="/login"
-                  onClick={handleNavLinkClick}
-                >
-                  Login
-                </a>
-              </li>
-              <li className="navItem">
-                <a
-                  className="navLink"
-                  href="/registration"
-                  onClick={handleNavLinkClick}
-                >
-                  Register
-                </a>
-              </li>
-            </>
-          )}
-        </ul>
-        <div
-          className={`navBurger ${burgerActive ? 'active' : ''}`}
-          onClick={handleBurgerClick}
-        >
-          <div className="barGroup">
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
+          <div
+            className={`navBurger ${burgerActive ? 'active' : ''}`}
+            onClick={handleBurgerClick}
+          >
+            <div className="barGroup">
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </div>
           </div>
         </div>
+
+        <div id="navbarEnd">{end}</div>
       </nav>
-      {children}
     </div>
   );
 };
