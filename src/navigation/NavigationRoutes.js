f5b877ed8589cded7444e2bda62491f983b31438
CJ <cliftonjones.87@gmail.com>
2023-07-03 22:04:29 -0400

    refactor: removed navbar and browser router from navigation routes component, changing navbar from accepting children to be rendered with props above other routes

--- src/navigation/NavigationRoutes.js
+++ src/navigation/NavigationRoutes.js
@@ -1,7 +1,6 @@
 import React, { lazy } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-const Navbar = lazy(() => import('../components/navbar/Navbar'));
 const LandingPage = lazy(() => import('../views/landing/LandingPage.jsx'));
 const Login = lazy(() => import('../views/authentication/Login.jsx'));
 const Registration = lazy(() =>
@@ -12,17 +11,11 @@ const NavigationRoutes = (props) => {
   const user = props.user;
 
   return (
-    <BrowserRouter>
-      <Navbar>
-        <Routes>
-          <>
-            <Route path="/" element={<LandingPage user={user} />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/registration" element={<Registration />} />
-          </>
-        </Routes>
-      </Navbar>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage user={user} />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
+    </Routes>
   );
 };
 
